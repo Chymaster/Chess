09855b4508d364d068bfafd70d3b552a3980f978
Xiaolin (Stuart) Cheng <a565837019@qq.com>
2017-09-12 19:21:26 +0100

    Rename PieceCentric_Board.py to PieceCentric_Board1.py

---
+++ PieceCentric_Board1.py
@@ -0,0 +1,3 @@
+global cb
+cb = [['wrl', 'wkl', 'wbl', 'wq', 'wk', 'wbr', 'wnr', 'wrr'], ['wp1', 'wp2', 'wp3', 'wp4', 'wp5', 'wp6', 'wp7', 'wp8'], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], ['bp1', 'bp2', 'bp3', 'bp4', 'bp5', 'bp6', 'bp7', 'bp8'], ['brl', 'bkl', 'bbl', 'bq', 'bk', 'bbr', 'bnr', 'brr']]
+#cb  is the orginal set up. cb stand for chessboard
