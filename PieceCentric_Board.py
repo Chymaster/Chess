04b57e2f123c494c0efc3f8aee5a98311bd81267
Xiaolin (Stuart) Cheng <a565837019@qq.com>
2017-09-12 12:09:27 +0100

    Create PieceCentric_Board.py

---
+++ PieceCentric_Board.py
@@ -0,0 +1,63 @@
+    global lbr   #each list is one piece in format
+    lbr = ['lbr',1,8,'r']   
+    global lbk
+    lbk = ['lbk',2,8,'n']       
+    global lbb
+    lbb = ['lbb',3,8,'b']           
+    bq = ['bq ',4,8,'q']            
+    bk = ['bk ',5,8,'k']         
+    global rbb
+    rbb = ['rbb',6,8,'b']       
+    global rbk
+    rbk = ['rbk',7,8,'n']
+    global rbr
+    rbr = ['rbr',8,8,'r']
+    global bp1
+    bp1 = ['bp1',1,7,'bp']
+    global bp2
+    bp2 = ['bp2',2,7,'bp']
+    global bp3
+    bp3 = ['bp3',3,7,'bp']
+    global bp4
+    bp4 = ['bp4',4,7,'bp']
+    global bp5
+    bp5 = ['bp5',5,7,'bp']
+    global bp6
+    bp6 = ['bp6',6,7,'bp']
+    global bp7
+    bp7 = ['bp7',7,7,'bp']
+    global bp8
+    bp8 = ['bp8',8,7,'bp']
+
+    global lwr
+    lwr = ['lwr',1,1,'r']
+    global lwk
+    lwk = ['lwk',2,1,'n']
+    global lwb
+    lwb = ['lwb',3,1,'b']
+    global wq
+    wq = ['wq ',4,1,'q']
+    global wk
+    wk = ['wk ',5,1,'k']
+    global rwb
+    rwb = ['rwb',6,1,'b']
+    global rwk
+    rwk = ['rwk',7,1,'n']
+    global rwr
+    rwr = ['rwr',8,1,'r']
+    global wp1
+    wp1 = ['wp1',1,2,'wp']
+    global wp2
+    wp2 = ['wp2',2,2,'wp']
+    global wp3
+    wp3 = ['wp3',3,2,'wp']
+    global wp4
+    wp4 = ['wp4',4,2,'wp']
+    global wp5
+    wp5 = ['wp5',5,2,'wp']
+    global wp6
+    wp6 = ['wp6',6,2,'wp']
+    global wp7
+    wp7 = ['wp7',7,2,'wp']
+    global wp8
+    wp8 = ['wp8',8,2,'wp']
